44565030d79b029a64d1420ff6728d9af9f6e6a4
Manikumar Pula <manikumarpula@gmail.com>
2025-08-13 18:48:13 +0530

    Fix : Autherization issue fixed

--- api/http/middlewares/autherization.middleware.go
+++ api/http/middlewares/autherization.middleware.go
@@ -1,22 +1,42 @@
 package middlewares
 
 import (
+	"errors"
 	"net/http"
-	"rate-limiter/api/http/dtos"
+	"rate-limiter/configs"
+	"strings"
+	"rate-limiter/internal/auth"
 
 	"github.com/labstack/echo/v4"
 )
 
-func AutherizationMiddleware(next echo.HandlerFunc) echo.HandlerFunc {
+func AutherizationMiddleware(next echo.HandlerFunc,config *config.EnvConfig) echo.HandlerFunc {
 	return func(c echo.Context) error {
-			token := c.Request().Header.Get("Authorization")
-			if token == "" {
-				return c.JSON(http.StatusUnauthorized, dtos.BaseResponse{
-					Status:  "error",
-					Message: "Unauthorized",
-					Error:   "Unauthorized",
-				})
-			}
+		// Check for presence of Authorization header
+		authHeader := c.Request().Header.Get("Authorization")
+		if authHeader == "" {
+			return c.JSON(http.StatusUnauthorized, errors.New("authorization header is missing"))
+		}
+
+		// Extract token from Bearer format
+		if !strings.HasPrefix(authHeader, "Bearer ") {
+			return c.JSON(http.StatusUnauthorized, errors.New("invalid authorization format"))
+		}
+
+		// Trim the Bearer prefix
+		tokenString := strings.TrimPrefix(authHeader, "Bearer ")
+		if tokenString == "" {
+			return c.JSON(http.StatusUnauthorized, errors.New("jwt token is missing"))
+		}
+
+		// Validate token
+		jwtService := auth.NewJwtService(config.SecretKey, config.SecretKey)
+
+		_, err := jwtService.DecodeToken(tokenString)
+		if err != nil {
+			return c.JSON(http.StatusUnauthorized, errors.New("invalid token"))
+		}
+
 		return next(c)
 	}
 }
--- api/http/routes/rate_limiter.route.go
+++ api/http/routes/rate_limiter.route.go
@@ -3,15 +3,18 @@ package routes
 import (
 	"rate-limiter/api/http/handlers"
 	"rate-limiter/api/http/middlewares"
+	config "rate-limiter/configs"
 
 	"github.com/labstack/echo/v4"
 )
 
-func SetupRateLimiterRoutes(e *echo.Echo) {
+func SetupRateLimiterRoutes(e *echo.Echo, config config.EnvConfig) {
 	api := e.Group("/api")
 
 	// apply rate limit middleware on all routes
-	api.Use(middlewares.AutherizationMiddleware)
+	api.Use(func(next echo.HandlerFunc) echo.HandlerFunc {
+		return middlewares.AutherizationMiddleware(next, &config)
+	})
 
 	api.GET("/rate-limit", handlers.RateLimitHandler)
 
